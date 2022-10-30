ba9a7fc5b89df3a520d739a4268888fc1057a3e3
dianlujitao <dianlujitao@lineageos.org>
2022-10-30 05:13:43 +0000

    RMX1801: Remove libbthost_if from product packages
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -170,7 +170,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     audio.bluetooth.default \
     libbluetooth_audio_session \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
